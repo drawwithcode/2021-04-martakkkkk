00b4b8e9b7c0c4d952b06c338261c6d2d903a340
marta <90717336+martakkkkk@users.noreply.github.com>
2021-11-10 10:29:58 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,55 @@
+var msg;
+var bgcolor;
+var button;
+var cnv;
+
+let imgs = [];
+let myColors = ["yellow", "pink", "purple"];
+
 function preload() {
-  // put preload code here
+  for (var i = 0; i < 10; i++) {
+    imgs[i] = loadImage("assets/img_" + i + ".jpg");
+  }
+}
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  cnv = createCanvas(windowWidth, windowHeight);
+  centerCanvas();
+
+  msg = createP("Clicca lo schermo per iniziare al meglio la tua giornata !");
+  msg.position(windowWidth / 2.48, windowHeight / 2);
+
+  imageMode(CENTER);
+
+  button = createButton(":)");
+  button.position(windowWidth / 2.25, windowHeight / 1.8);
+  button.mousePressed(changeColor);
+  button.style("color:white");
+  button.style("background:red");
+  button.size(200, 50);
+}
+
+function mouseClicked() {
+  var index = int(random(10));
+  image(imgs[index], mouseX, mouseY, 950, 500);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  background(bgcolor);
 }
+
+function changeColor() {
+  //bgcolor = color(random(255));
+  bgcolor = color(random(0, 255));
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
